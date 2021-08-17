19cef792ea793469da4dbe87035e28f9c93c6407
yuvaraj <yuvrajmu@gmail.com>
2021-08-17 19:08:42 +0530

    first commit

---
+++ selenium project/src/Launch.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.gmail.com");
+		String curl= driver.getCurrentUrl();
+		System.out.println(curl);
+		
+		//Thread.sleep(5000);
+		//driver.close();
+		
+
+	}
+
+}
